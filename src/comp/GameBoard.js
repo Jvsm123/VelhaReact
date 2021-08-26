6cc45ab76d63e938b92c842a96a1156da09508de
Jvsm123 <Jvsm123@outlook.com>
2021-08-26 18:05:22 -0300

    definindo master

--- src/comp/GameBoard.js
+++ src/comp/GameBoard.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 export default function App()
 {
-	let style =
+	const style =
 	{
-		
+		display: 'flex',
+		width: '35rem',
+		flexWrap: 'wrap',
+	};
+
+	const btnStyle =
+	{
+		backgroundColor: 'gray',
+		padding: '4.5rem',
+		border: '1px solid black',
+		borderRadius: '2px',
+		cursor: 'pointer',
+		color: 'white',
 	};
 
 	return(
 		<div style={style}>
-		
+
 		</div>
 	);
 };
