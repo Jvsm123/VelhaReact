d43a8b28324ca360a7f605eb11d0dadcc3cf65d1
Jvsm123 <Jvsm123@outlook.com>
2021-08-26 18:01:02 -0300

    master update

---
+++ src/comp/GameBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function App()
+{
+	let style =
+	{
+		
+	};
+
+	return(
+		<div style={style}>
+		
+		</div>
+	);
+};
---
+++ src/comp/Placar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Placar()
+{
+	return(
+		<>
+			<h1>Aqui há um Placar!</h1>
+		</>
+	);
+};
