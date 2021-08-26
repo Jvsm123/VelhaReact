8346d80815a3e27fa218d4fcd2c1253e2028f038
Jvsm123 <Jvsm123@outlook.com>
2021-08-26 19:00:29 -0300

    criação dos botões

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import Placar from './comp/Placar';
 
 export default function App()
 {
+	const style =
+	{
+		display: 'flex',
+		heigth: '100vh',
+	};
+
 	return(
-		<>
+		<div style={style}>
 			<GameBoard/>
 			<Placar/>
-		</>
+		</div>
 	);
 };
--- src/comp/GameBoard.js
+++ src/comp/GameBoard.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function App()
+export default function GameBoard()
 {
+	const [btn1, setBtn1] = useState(1);
+	const [btn2, setBtn2] = useState(2);
+	const [btn3, setBtn3] = useState(3);
+	const [btn4, setBtn4] = useState(4);
+	const [btn5, setBtn5] = useState(5);
+	const [btn6, setBtn6] = useState(6);
+	const [btn7, setBtn7] = useState(7);
+	const [btn8, setBtn8] = useState(8);
+
 	const style =
 	{
 		display: 'flex',
@@ -19,9 +28,28 @@ export default function App()
 		color: 'white',
 	};
 
+	function board()
+	{
+		let btn = [];
+
+		for(let i = 1; i < 10; i++)
+		{
+			btn.push(
+				<button
+					style={btnStyle}
+					onClick={() => `${setBtn1('teste')}`}
+				>
+					`${btn}`
+				</button>
+			);
+		}
+
+		return btn;
+	}
+
 	return(
 		<div style={style}>
-
+			{board()}
 		</div>
 	);
 };
