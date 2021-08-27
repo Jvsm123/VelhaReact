3be4755dd2510d3c2ae70a4e4a064a8ac9446760
Jvsm123 <Jvsm123@outlook.com>
2021-08-27 19:33:54 -0300

    Lógica central(Mal Feita!!!)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ export default function App()
 	const [vez, setVez] = useState(true);
 	const [total, setTotal] = useState(0);
 	const [placar, setPlacar] = useState(0);
+	const [vitoria, setVitoria] = useState(
+	[
+		[], [], [],
+		[], [], [],
+		[], [], [],
+	]);
+
+	const resetBtn =
+	{
+		padding: '2rem',
+		fontSize: '20pt',
+		fontWeight: 'bold',
+	};
 
 	const board =
 	{
@@ -27,28 +40,37 @@ export default function App()
 	{
 		setVez(true);
 		setTotal(0);
+		setVitoria([ [], [], [], [], [], [], [], [], [] ]);
 	};
 
 	return(
 		<div style={style}>
-			{ total < 9
+			{
+				total <= 8
 				&&
 					<div style={board}>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
-						<GameBoard vez={vez} setVez={setVez} total={total} setTotal={setTotal}/>
+						<GameBoard bt={0} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
+						<GameBoard bt={1} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
+						<GameBoard bt={2} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
+						<GameBoard bt={3} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
+						<GameBoard bt={4} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
+						<GameBoard bt={5} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
+						<GameBoard bt={6} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
+						<GameBoard bt={7} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
+						<GameBoard bt={8} vez={vez} setVez={setVez} total={total} setTotal={setTotal} vitoria={vitoria} setVitoria={setVitoria}/>
 					</div>
 				||
-					<div>
-						<h1>O Jogo acabou!</h1>
-						<button onClick={() => reset()}>Recomeçar?</button>
+				total === 10
+				&&
+					<div >
+						<h1>Jogador Tal é o Vencedor!!!</h1>
+						<button style={resetBtn} onClick={() => reset()}>Recomeçar?</button>
 						<Placar placar={placar} setPlacar={setPlacar}/>
+					</div>
+				||
+					<div >
+						<h1>VELHA!!!</h1>
+						<button style={resetBtn} onClick={() => reset()}>Recomeçar?</button>
 					</div>
 			}
 		</div>
--- src/comp/GameBoard.js
+++ src/comp/GameBoard.js
@@ -13,22 +13,125 @@ export default function GameBoard(props)
 		borderRadius: '2px',
 		cursor: 'pointer',
 		color: 'white',
+		fontSize: '14pt',
+		fontWeight: 'bold',
+		textAlign: 'center',
 	};
 
 	function jogador()
 	{
 		if(!lock)
 		{
-			( props.vez ) ? setBtn('O') : setBtn('X');
+			let t;
+
+			if( props.vez )
+			{
+				setBtn('O');
+				t = 'O'
+			}
+			else
+			{
+				setBtn('X');
+				t = 'X';
+			}
 
 			props.setVez( !props.vez );
 
 			setLock(!lock);
-			
+
 			props.setTotal( props.total + 1 );
 
-			console.log(props.total);
+			verificaVitoria(t);
 		}
+	};
+
+	function verificaVitoria(t)
+	{
+		props.vitoria[props.bt] = t;
+
+		if( props.vitoria[0] === 'X' && props.vitoria[1] === 'X' && props.vitoria[2] === 'X' )
+		{
+			console.log(props.vitoria[0] + '1 Vertical')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[3] === 'X' && props.vitoria[4] === 'X' && props.vitoria[5] === 'X')
+		{
+			console.log(props.vitoria[3] + ' 2 Vertical')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[6] === 'X' && props.vitoria[7] === 'X' && props.vitoria[8] === 'X')
+		{
+			console.log(props.vitoria[6] + ' 3 Vertical')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[0] === 'X' && props.vitoria[3] === 'X' && props.vitoria[6] === 'X')
+		{
+			console.log(props.vitoria[0] + ' 1 Horizontal')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[1] === 'X' && props.vitoria[4] === 'X' && props.vitoria[7] === 'X')
+		{
+			console.log(props.vitoria[1] + ' 2 Horizontal')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[2] === 'X' && props.vitoria[5] === 'X' && props.vitoria[8] === 'X')
+		{
+			console.log(props.vitoria[2] + ' 3 Horizontal')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[0] === 'X' && props.vitoria[4] === 'X' && props.vitoria[8] === 'X')
+		{
+			console.log(props.vitoria[0] + ' 1 Diagonal')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[2] === 'X' && props.vitoria[4] === 'X' && props.vitoria[6] === 'X')
+		{
+			console.log(props.vitoria[6] + ' 2 Diagonal')
+			props.setTotal(10);
+		}
+
+		if( props.vitoria[0] === 'O' && props.vitoria[1] === 'O' && props.vitoria[2] === 'O' )
+		{
+			console.log(props.vitoria[0] + '1 Vertical')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[3] === 'O' && props.vitoria[4] === 'O' && props.vitoria[5] === 'O')
+		{
+			console.log(props.vitoria[3] + ' 2 Vertical')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[6] === 'O' && props.vitoria[7] === 'O' && props.vitoria[8] === 'O')
+		{
+			console.log(props.vitoria[6] + ' 3 Vertical')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[0] === 'O' && props.vitoria[3] === 'O' && props.vitoria[6] === 'O')
+		{
+			console.log(props.vitoria[0] + ' 1 Horizontal')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[1] === 'O' && props.vitoria[4] === 'O' && props.vitoria[7] === 'O')
+		{
+			console.log(props.vitoria[1] + ' 2 Horizontal')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[2] === 'O' && props.vitoria[5] === 'O' && props.vitoria[8] === 'O')
+		{
+			console.log(props.vitoria[2] + ' 3 Horizontal')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[0] === 'O' && props.vitoria[4] === 'O' && props.vitoria[8] === 'O')
+		{
+			console.log(props.vitoria[0] + ' 1 Diagonal')
+			props.setTotal(10);
+		}
+		else if( props.vitoria[2] === 'O' && props.vitoria[4] === 'O' && props.vitoria[6] === 'O')
+		{
+			console.log(props.vitoria[6] + ' 2 Diagonal')
+			props.setTotal(10);
+		}
+
+		console.log(props.vitoria, props.total);
 	};
 
 	return(
